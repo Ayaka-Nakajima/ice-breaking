9c577aeb2b74ad155abd6c6fecb18f5f5414417b
Alisa <146712422+iggyw1g@users.noreply.github.com>
2025-09-04 23:26:11 +0900

    updated

--- problems/__init__.py
+++
@@ -1,4 +0,0 @@
-from .algorithm_problems import algo_problems
-from .for_loop_problems import for_loop_problems
-from .if_else_problems import if_else_problems
-from .recursion_problems import recursion_problems
